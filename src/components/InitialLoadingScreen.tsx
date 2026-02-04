44372aefc8e67719ea8a0a1e768e9dacff9a276e
陳曦 <eriksonchen@gmail.com>
2026-02-04 17:15:26 +0800

    feat: enlarge initial loading icon to align with pwa splash

--- src/components/InitialLoadingScreen.tsx
+++ src/components/InitialLoadingScreen.tsx
@@ -5,13 +5,11 @@ import './InitialLoadingScreen.css';
 export function InitialLoadingScreen() {
     return (
         <div className='initial-loading-screen'>
-            <div className='initial-loading-content'>
-                <TrainFront
-                    className='initial-loading-icon'
-                    size={64}
-                    strokeWidth={2}
-                />
-            </div>
+            <TrainFront
+                className='initial-loading-icon'
+                size={100}
+                strokeWidth={2}
+            />
         </div>
     );
 }
